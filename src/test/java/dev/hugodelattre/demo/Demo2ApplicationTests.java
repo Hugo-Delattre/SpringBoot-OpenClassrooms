250000b0180839411c56fd3f03a3f7ae05cfcd55
Hugo Delattre <delattre.hugo.24@gmail.com>
2024-04-10 16:27:59 +0200

    initial commit

---
+++ src/test/java/dev/hugodelattre/demo/Demo2ApplicationTests.java
@@ -0,0 +1,14 @@
+package dev.hugodelattre.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class Demo2ApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+
+}
